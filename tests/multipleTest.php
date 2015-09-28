4ba594b3ad0cbdbdbcd17447df7fc2eb89630051
Neo <call@woola.net>
2015-09-28 22:47:12 +0800

    Fixed 简单调整测试

--- tests/multipleTest.php
+++ tests/multipleTest.php
@@ -1,7 +1,5 @@
 <?php
 
-require '../vendor/autoload.php';
-
 use Yoozi\LBS\Query\Place;
 use Yoozi\LBS\Search;
 
@@ -9,16 +7,8 @@ class multipleTest extends PHPUnit_Framework_TestCase
 {
     public function testPushAndPop()
     {
-
 		$place = new Place;
-		$place->set('method', 'suggestion');
-		$place->set('q', '尚德大厦');
-		$place->set('region', '广州市');
-
-		$search = new Search('E4805d16520de693a3fe707cdc962045', $place);
-
-		var_dump($search->run()->toArray());
-
+		$this->assertTrue(isset($place));
     }
 }
 
